463cd2ed7f907472f0dacff713e56aea9ac0d541
Ashleigh Christopher <ashleighc207@gmail.com>
2019-09-24 16:31:22 -0400

    Updates

---
+++ precache-manifest.5b2c86af4a2852fc4d4cd01c4c60caed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4504a5c3874919b4f5c7c77f6ae9dea6",
+    "url": "/react-card-deck/index.html"
+  },
+  {
+    "revision": "af59c558507697cad655",
+    "url": "/react-card-deck/static/css/main.5756f4aa.chunk.css"
+  },
+  {
+    "revision": "041c379a700a16dca619",
+    "url": "/react-card-deck/static/js/2.0365823c.chunk.js"
+  },
+  {
+    "revision": "af59c558507697cad655",
+    "url": "/react-card-deck/static/js/main.c697190b.chunk.js"
+  },
+  {
+    "revision": "3498e8901873ab234ae6",
+    "url": "/react-card-deck/static/js/runtime-main.c21e4ce9.js"
+  }
+]);
