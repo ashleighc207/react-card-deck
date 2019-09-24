428a49f1853cb25d221da24fe863c9814b7eaabb
Ashleigh Christopher <ashleighc207@gmail.com>
2019-09-24 16:10:57 -0400

    add in key prop and alt tag for images, remove useless constructor

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -24,8 +24,10 @@ class Board extends Component {
     const url = `https://deckofcardsapi.com/api/deck/${this.state.id}/draw/`;
 
     axios.get(url).then(response => {
-      let imageUrl = response.data.cards[0].image;
-      let rotate;
+      let imageUrl = response.data.cards[0].image,
+        cardId = response.data.cards[0].code,
+        altTxt = `${response.data.cards[0].value} ${response.data.cards[0].suit}`,
+        rotate;
       if (Math.floor(100 * Math.random()) <= 50) {
         rotate = Math.random() * 30 - 5;
       } else {
@@ -33,7 +35,14 @@ class Board extends Component {
       }
       let x = Math.floor(50 * Math.random());
       let y = Math.floor(50 * Math.random());
-      let newCard = { img: imageUrl, x: x, y: y, rotate: rotate };
+      let newCard = {
+        img: imageUrl,
+        x: x,
+        y: y,
+        rotate: rotate,
+        id: cardId,
+        altText: altTxt
+      };
       this.setState(prevState => {
         return {
           deck: [...this.state.deck, newCard],
@@ -60,6 +69,8 @@ class Board extends Component {
                 x={card.x}
                 rotate={card.rotate}
                 y={card.y}
+                key={card.id}
+                alt={card.altText}
               />
             );
           })}
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,14 +2,11 @@ import React, { Component } from "react";
 import "./Card.css";
 
 class Card extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <img
         className="Card"
+        alt={this.props.alt}
         src={this.props.image}
         style={{
           transform: `translateX(${this.props.x}px) translateY(${this.props.y}px) rotate(${this.props.rotate}deg)`
